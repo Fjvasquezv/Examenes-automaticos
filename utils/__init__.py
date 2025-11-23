9917527bfb89cc69505741b03d58db50265a49a7
Fjvasquezv <34523799+Fjvasquezv@users.noreply.github.com>
2025-11-23 17:49:51 -0500

    Add files via upload

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,26 +2,5 @@
 Módulo utils - Utilidades y validadores
 """
 
-from .validators import (
-    validate_codigo_estudiante,
-    validate_respuesta,
-    validate_nivel,
-    validate_nota,
-    validate_pregunta_estructura,
-    validate_config_estructura,
-    validate_respuestas_lista,
-    sanitize_texto,
-    validate_spreadsheet_id
-)
+__version__ = "1.0.0"
 
-__all__ = [
-    'validate_codigo_estudiante',
-    'validate_respuesta',
-    'validate_nivel',
-    'validate_nota',
-    'validate_pregunta_estructura',
-    'validate_config_estructura',
-    'validate_respuestas_lista',
-    'sanitize_texto',
-    'validate_spreadsheet_id'
-]
