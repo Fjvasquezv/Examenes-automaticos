48ce36d6b7236778b1dad7ddab00b48837a2f782
Fjvasquezv <34523799+Fjvasquezv@users.noreply.github.com>
2025-11-23 18:29:45 -0500

    Update __init__.py

--- scr/__init__.py
+++ scr/__init__.py
@@ -3,5 +3,19 @@ Módulo src - Sistema de Examen Adaptativo
 """
 
 __version__ = "1.0.0"
-__author__ = "Sistema de Examen Adaptativo"
 
+from .config_loader import ConfigLoader
+from .question_manager import QuestionManager
+from .scoring_systems import crear_sistema_calificacion
+from .exam_logic import ExamLogic
+from .ui_components import UIComponents
+from .data_persistence import DataPersistence
+
+__all__ = [
+    'ConfigLoader',
+    'QuestionManager',
+    'crear_sistema_calificacion',
+    'ExamLogic',
+    'UIComponents',
+    'DataPersistence'
+]
