4b29a014b1f4fb6065edf4053ad4bf97e516d752
Fjvasquezv <34523799+Fjvasquezv@users.noreply.github.com>
2025-11-24 11:29:54 -0500

    Update ui_components.py

--- src/ui_components.py
+++ src/ui_components.py
@@ -30,7 +30,7 @@ class UIComponents:
         
         st.markdown(f"""
         <div style='background-color: #FFB81C;
-             padding: 20px 20px 8px 20px;
+             padding: 30px 20px 8px 20px;
              border-bottom: 4px solid #003366;
              margin-bottom: 2px;
              display: flex; justify-content: space-between;'>
